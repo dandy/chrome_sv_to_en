36c7422e5e9faa1e4618e73c5d35f2fd5dd9d243
M. Ijlal Hasnain <ijlal2@gmail.com>
2013-11-29 11:36:28 +0100

    Changed function names

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function sendSearch(selectedText) {
-    var serviceCall = 'http://api.microsofttranslator.com/V2/Ajax.svc/Translate?appId=8B841CA7C1A03443682C52AD07B7775A7BD5B3AA&from=sv&to=en&text=' + selectedText;
-    httpGetUrl(serviceCall);
+function SendToTranslate(selectedText) {
+    var apiCall = 'http://api.microsofttranslator.com/V2/Ajax.svc/Translate?appId=8B841CA7C1A03443682C52AD07B7775A7BD5B3AA&from=sv&to=en&text=' + selectedText;
+    httpGetUrl(apiCall);
 }
 
 chrome.contextMenus.create({
     "title": "Translate to English",
     "contexts": ["selection"],
     "onclick": function (info, tab) {
-        sendSearch(info.selectionText);
+        SendToTranslate(info.selectionText);
     }
 });
 
